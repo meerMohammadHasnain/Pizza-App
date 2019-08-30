6d5a53216cdf88f9aded9ec4f3bc060d20920cee
meerMohammadHasnain <54717728+meerMohammadHasnain@users.noreply.github.com>
2019-08-30 22:06:08 +0530

    Add files via upload

---
+++ userMicroservice/config/default-01.js
@@ -0,0 +1,14 @@
+// Dependencies
+import 'dotenv/config';
+import path from 'path';
+
+// Export the configuration object
+export var config = {
+  'httpPort' : 1000,
+  'httpsPort' : 1001,
+  'consulInfo' : {
+    'registeredServiceIdHttp' : 'user_microservice_HTTP_01',
+    'registeredServiceIdHttps' : 'user_microservice_HTTPS_01'
+  },
+  'logFileName' : path.join(__dirname, "/../.logs/user_microservice01.log")
+};
---
+++ userMicroservice/config/development-02.js
@@ -0,0 +1,14 @@
+// Dependencies
+import 'dotenv/config';
+import path from 'path';
+
+// Export the configuration object
+export var config = {
+  'httpPort' : 1002,
+  'httpsPort' : 1003,
+  'consulInfo' : {
+    'registeredServiceIdHttp' : 'user_microservice_HTTP_02',
+    'registeredServiceIdHttps' : 'user_microservice_HTTPS_02'
+  },
+  'logFileName' : path.join(__dirname, "/../.logs/user_microservice02.log"),
+};
